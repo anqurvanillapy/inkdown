b5fd464da3078b285c806784119cfec0d37634fb
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-05-21 01:25:29 +0800

    use README.md as specs itself

--- README.md
+++ README.md
@@ -7,9 +7,42 @@ This repository contains a minimal script in JS for creating the project page,
 demostrating both source of specs (Markdown) and some handwriting-style output
 (HTML).
 
-## Syntax
+## Supported syntaxes
 
-TODO
+### Headers
+
+Headers are prefixed with `X`s.
+
+# Header 1
+## Header 2
+### Header 3
+#### Header 4
+##### Header 5
+###### Header 6
+
+### Text styles
+
+**Strong** texts are underlined double, and used for highlighting some
+**terminologies**. *Italic* texts are underline wavy, for emphasizing *anything
+important*. ~~Deleted~~ texts are strikethroughed-ed, that's the way it is.
+
+### Ordered lists
+
+1. Item 1
+2. Item 2
+3. Item 3
+
+### Unordered lists
+
+- Item
+- Another item
+- And, an item
+    + Subitem it is
+    + Another subitem
+
+### TODOs
+
+`code`, unordered lists and more.
 
 ## License
 
--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 import marked from 'marked'
-import doT from 'dot'
 import fs from 'fs'
 
-let [mdtxt, tmpl] = [
-  '/dev/stdin', 'template.html'
-].map(f => fs.readFileSync(f).toString())
-
-let content = marked(mdtxt)
-let tmplFn = doT.template(tmpl)
-let output = tmplFn({content: content})
+const text = fs.readFileSync('README.md', 'utf-8')
+const payload = marked(text)
+let output =
+`<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="initial-scale=1">
+  <meta name="description" content="Markdown for handwriting">
+  <meta name="author" content="AnqurVanillapy">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Nothing+You+Could+Do">
+  <title>Inkdown</title>
+</head>
+<body>
+  <section>
+    <pre class="text">${text}</pre>
+  </section>
+  <section>
+    ${payload}
+  </section>
+  </body>
+</html>`
 
 fs.writeFileSync('index.html', output)
